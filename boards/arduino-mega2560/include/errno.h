4e3666bddccb1dc7febb57092f7c147d920b2a5e
Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
2014-05-20 22:48:17 +0200

    Arduino Mega 2560: strip functions in atmega2560/periph and atmega_common/thread_arch.c down to prototypes  - Add custom errno.h since avr-libc doesn't include one

---
+++ boards/arduino-mega2560/include/errno.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @defgroup    board_arduino-mega2560 Arduino Mega 2560
+ * @ingroup     boards
+ * @brief       Board specific files for the Arduino Mega 2560 board.
+ * @{
+ *
+ * @file        board.h
+ * @brief       Board specific definitions for the Arduino Mega 2560 board.
+ *
+ * @author      Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
+ */
+#ifndef __ERRNO_H
+#define __ERRNO_H
+
+#define	EBUSY		16	/* Device or resource busy */
+#define EINVAL      22  /* Invalid argument */
+#define	EOVERFLOW	75	/* Value too large for defined data type */
+
+#endif /** __ERRNO_H */
+/** @} */
