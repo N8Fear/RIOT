56d202f57c141151baaaa355b73874f1a8887b04
Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
2014-06-10 13:25:42 +0200

    Arduino Mega 2560: add Macros to get and set the global interrupt flag

--- cpu/atmega_common/include/cpu.h
+++ cpu/atmega_common/include/cpu.h
@@ -7,25 +7,27 @@
  */
 
 /**
- * @defgroup    cpu_cortexm_common ARM Cortex-M common files
+ * @defgroup    cpu_atmega_common ATmega common files
  * @ingroup     cpu
- * @brief       Common implementations and headers for Cortex-M family based micro-controllers
+ * @brief       Common implementations and headers for ATmega family based micro-controllers
  * @{
  *
  * @file        cpu.h
- * @brief       Basic definitions for the Cortex-M common module
+ * @brief       Basic definitions for the ATmega common module
  *
  * When ever you want to do something hardware related, that is accessing MCUs registers directly,
  * just include this file. It will then make sure that the MCU specific headers are included.
  *
  * @author      Stefan Pfeiffer <stefan.pfeiffer@fu-berlin.de>
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
  */
 
-#ifndef __CORTEXM_COMMON_H
-#define __CORTEXM_COMMON_H
+#ifndef __ATMEGA_COMMON_H
+#define __ATMEGA_COMMON_H
 
 #include "cpu-conf.h"
+#include <avr/interrupt.h>
 
 /**
  * For downwards compatibility with old RIOT code.
@@ -36,14 +38,37 @@
 #define dINT            disableIRQ
 
 /**
+ * @brief Macro returns state of the global interrupt register
+ */
+#define __get_interrupt_state() ({            \
+        unsigned char sreg;                   \
+        asm volatile ("in r0, __SREG__; \n\t" \
+                      "mov %0, r0"            \
+                      : "=g" (sreg)           \
+					  :	                      \
+		              : "r0");                \
+        sreg&(1<<7);                          \
+})
+
+#define __set_interrupt_state(X) ({           \
+	    asm volatile ("mov r15,%0;       \n\t"\
+				      "in r16, __SREG__; \n\t"\
+					  "cbr r16,7;        \n\t"\
+				      "and r15,r16;      \n\t"\
+				      "out __SREG__, r15 \n\t"\
+				      :                       \
+				      : "g" (X)               \
+				      : "r15","r16");         \
+})
+/**
  * @brief Macro has to be called in the beginning of each ISR
  */
-#define ISR_ENTER()         asm("push {LR}")
+//#define ISR_ENTER()         asm("push {LR}")
 
 /**
  * @brief Macro has to be called on each exit of an ISR
  */
-#define ISR_EXIT()          asm("pop {r0}"); asm("bx r0")
+//#define ISR_EXIT()          asm("pop {r0}"); asm("bx r0")
 
 /**
  * @brief Initialization of the CPU
@@ -51,5 +76,5 @@
 void cpu_init(void);
 
 
-#endif /* __CORTEXM_COMMON_H */
+#endif /* __ATMEGA_COMMON_H */
 /** @} */
