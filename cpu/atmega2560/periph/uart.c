91d209b6f9441af8b6cbf695099aaa04c5d6e50d
Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
2014-06-30 12:29:07 +0200

    Arduino Mega 2560: finish implementation of uart interface

--- cpu/atmega2560/periph/uart.c
+++ cpu/atmega2560/periph/uart.c
@@ -57,9 +57,26 @@ int uart_init(uart_t uart, uint32_t baudrate, void (*rx_cb)(char), void (*tx_cb)
 
 	/* configure interrupts and enable RX interrupt */
 	switch (uart) {
+#if UART_0_EN
 		case UART_0:
 			UCSR0B |= (1 << RXCIE0);
-		break;
+			break;
+#endif /* UART_0_EN */
+#if UART_1_EN
+		case UART_1:
+			UCSR1B |= (1 << RXCIE1);
+			break;
+#endif /* UART_1_EN */
+#if UART_2_EN
+		case UART_2:
+			UCSR2B |= (1 << RXCIE2);
+			break;
+#endif /* UART_2_EN */
+#if UART_3_EN
+		case UART_3:
+			UCSR3B |= (1 << RXCIE3);
+			break;
+#endif /* UART_3_EN */
 		case UART_UNDEFINED:
 			return -2;
 	}
@@ -84,18 +101,39 @@ int uart_init_blocking(uart_t uart, uint32_t baudrate)
 #endif /* UART_0 */
 #if UART_1_EN
 		case UART_1:
+			/* enable RX and TX */
+			UCSR1B |= (1 << RXEN1) | (1 << TXEN1);
+			/* use 8 Bit characters */
+			UCSR1C |= (1 << UCSZ10) | (1 << UCSZ11);
 
+			/* set clock divider */
+			UBRR1L = clock_divider;
+			UBRR1H = (clock_divider >> 8);
 			break;
 #endif /* UART_1 */
 #if UART_2_EN
 		case UART_2:
+			/* enable RX and TX */
+			UCSR2B |= (1 << RXEN2) | (1 << TXEN2);
+			/* use 8 Bit characters */
+			UCSR2C |= (1 << UCSZ20) | (1 << UCSZ21);
 
+			/* set clock divider */
+			UBRR2L = clock_divider;
+			UBRR2H = (clock_divider >> 8);
 			break;
 #endif /* UART_2 */
 #if UART_3_EN
 		case UART_3:
+			/* enable RX and TX */
+			UCSR3B |= (1 << RXEN3) | (1 << TXEN3);
+			/* use 8 Bit characters */
+			UCSR3C |= (1 << UCSZ30) | (1 << UCSZ31);
 
-			break
+			/* set clock divider */
+			UBRR3L = clock_divider;
+			UBRR3H = (clock_divider >> 8);
+			break;
 #endif /* UART_3 */
 		case UART_UNDEFINED:
 			return -2;
@@ -116,15 +154,95 @@ void uart_tx_end(uart_t uart)
 
 int uart_write(uart_t uart, char data)
 {
+	switch (uart) {
+#if UART_0_EN
+		case UART_0:
+			UDR0 = data;
+			break;
+#endif /* UART_0_EN */
+#if UART_1_EN
+		case UART_1:
+			UDR1 = data;
+			break;
+#endif /* UART_1_EN */
+#if UART_2_EN
+		case UART_2:
+			UDR2 = data;
+			break;
+#endif /* UART_2_EN */
+#if UART_3_EN
+		case UART_3:
+			UDR3 = data;
+			break;
+#endif /* UART_3_EN */
+		case UART_UNDEFINED:
+			return -1;
+	}
     return 1;
 }
 
 int uart_read_blocking(uart_t uart, char *data)
 {
+	switch (uart) {
+#if UART_0_EN
+		case UART_0:
+			while (!(UCSR0A & (1 << RXC0)));
+			*data = (char) UDR0;
+			break;
+#endif /* UART_0_EN */
+#if UART_1_EN
+		case UART_1:
+			while (!(UCSR1A & (1 << RXC1)));
+			*data = (char) UDR1;
+			break;
+#endif /* UART_1_EN */
+#if UART_2_EN
+		case UART_2:
+			while (!(UCSR2A & (1 << RXC2)));
+			*data = (char) UDR2;
+			break;
+#endif /* UART_2_EN */
+#if UART_3_EN
+		case UART_3:
+			while (!(UCSR3A & (1 << RXC3)));
+			*data = (char) UDR3;
+			break;
+#endif /* UART_3_EN */
+		case UART_UNDEFINED:
+			return -1;
+	}
     return 1;
 }
 
 int uart_write_blocking(uart_t uart, char data)
 {
+	switch (uart) {
+#if UART_0_EN
+		case UART_0:
+			while (!(UCSR0A & (1 << UDRE0)));
+			UDR0 = data;
+			break;
+#endif /* UART_0_EN */
+#if UART_1_EN
+		case UART_1:
+			while (!(UCSR1A & (1 << UDRE1)));
+			UDR1 = data;
+			break;
+#endif /* UART_1_EN */
+#if UART_2_EN
+		case UART_2:
+			while (!(UCSR2A & (1 << UDRE2)));
+			UDR2 = data;
+			break;
+#endif /* UART_2_EN */
+#if UART_3_EN
+		case UART_3:
+			while (!(UCSR3A & (1 << UDRE3)));
+			UDR3 = data;
+			break;
+#endif /* UART_3_EN */
+		case UART_UNDEFINED:
+			return -1;
+	}
     return 1;
 }
